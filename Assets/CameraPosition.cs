4ecfd32dfdb06ed7ba1ec093716cc8bb0dc909be
Andreas Fuchs <info@anfuchs.de>
2019-12-17 18:48:15 +0100

    first commit

---
+++ Assets/CameraPosition.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+
+
+    public Transform Top_View;
+    public Transform Side_View;
+    public Transform target;
+
+    bool side = true;
+
+    private Transform targetPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = Side_View;
+        transform.position = targetPosition.position;
+        Debug.Log(targetPosition + " / " + transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool down = Input.GetKeyDown(KeyCode.Space);
+        if (down & side)
+        {
+            side = false;
+            targetPosition = Top_View;
+        }
+        else if (down & !side)
+        {
+            side = true;
+            targetPosition = Side_View;
+        }
+
+        if (targetPosition.position != transform.position)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition.position, 0.2f);
+            Debug.Log("Lerping to Target");
+        }
+
+        transform.LookAt(target);
+    }
+
+}
