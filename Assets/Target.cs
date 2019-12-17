e0f101e8fb27a3f89bc31567310a5913771feb51
Andreas Fuchs <info@anfuchs.de>
2019-12-17 19:16:06 +0100

    cam follows in both views

--- Assets/Target.cs
+++ Assets/Target.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotate : MonoBehaviour
+public class Target : MonoBehaviour
 {
+
+
+    public Transform follow;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.eulerAngles += new Vector3(0, 1f, 0);
+        transform.position = Vector3.Lerp(transform.position, follow.position, 0.01f);
     }
 }
