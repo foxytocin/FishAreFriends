9dddc2f0828c462b1e1a569a360632867bc252a1
Andreas Fuchs <info@anfuchs.de>
2019-12-20 00:38:04 +0100

    chasing food

--- Assets/Scripts/MoveToMouseClick.cs
+++ Assets/Scripts/MoveToMouseClick.cs
@@ -1,49 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MoveToMouseClick : MonoBehaviour
 {
     public Vector3 target;
 
-    // Angular speed in radians per sec.
+    private FindNearestGameObject findStuff;
+    private bool goingForFood;
+    GameObject closestFood = null;
+    Color color;
+
+
     public float speed = 3.0f;
 
-
-
-    void Update()
+    void Awake()
     {
-        int layerMask = 1 << 8;
-        layerMask = ~layerMask;
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        target = transform.position;
+        findStuff = FindObjectOfType<FindNearestGameObject>();
+        goingForFood = false;
+    }
 
-            if (Physics.Raycast(ray, out hit))
+    void LateUpdate()
+    {
+        if (!goingForFood)
+        {
+            closestFood = findStuff.FindClosestFood();
+            goingForFood = closestFood ? true : false;
+            if (goingForFood)
             {
-                //Debug.Log("HIT-POINT: " + hit.point);
-                target = hit.point + new Vector3(0, 2, 0);
+                var block = new MaterialPropertyBlock();
+                block.SetColor("_BaseColor", Color.green);
+                closestFood.GetComponent<Renderer>().SetPropertyBlock(block);
             }
         }
 
-        transform.position = Vector3.Lerp(transform.position, target, 0.5f * Time.deltaTime);
+        if (closestFood && goingForFood)
+        {
+            target = closestFood.transform.position;
+        }
+        else if (goingForFood && !closestFood)
+        {
+            goingForFood = false;
+            closestFood = null;
+        }
 
+        transform.position = Vector3.Lerp(transform.position, target, 0.5f * Time.deltaTime * 2);
 
-        // Determine which direction to rotate towards
         Vector3 targetDirection = target - transform.position;
-
-        // The step size is equal to speed times frame time.
         float singleStep = speed * Time.deltaTime;
-
-        // Rotate the forward vector towards the target direction by one step
         Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
-
-        // Draw a ray pointing at our target in
         Debug.DrawRay(transform.position, newDirection, Color.red);
-
-        // Calculate a rotation a step closer to the target and applies rotation to this object
         transform.rotation = Quaternion.LookRotation(newDirection);
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        goingForFood = false;
+        Destroy(closestFood);
+        closestFood = null;
     }
 }
