28590f8989eaf905d2898604f99a28083097550f
Andreas Fuchs <info@anfuchs.de>
2019-12-22 19:34:16 +0100

    food-behavior changed

---
+++ Assets/Scripts/EcoSystemManager/FoodBehavior.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class FoodBehavior : MonoBehaviour
+{
+
+    EcoSystemManager ecoSystemManager;
+    private float availableFood;
+    private BoxCollider b_collider;
+
+
+    void Awake()
+    {
+        ecoSystemManager = FindObjectOfType<EcoSystemManager>();
+        availableFood = Random.Range(500, 5000);
+        ecoSystemManager.setAvailableFood(availableFood);
+        b_collider = GetComponent<BoxCollider>();
+        scaleFood();
+    }
+
+
+    private void scaleFood()
+    {
+        float size = availableFood / 1000f;
+        transform.localScale = new Vector3(size, size, size);
+        b_collider.size = new Vector3(size, size, size);
+    }
+
+    public float getFood(float amount)
+    {
+        if (availableFood >= amount)
+        {
+            availableFood -= amount;
+            ecoSystemManager.setAvailableFood(-amount);
+            scaleFood();
+            //Debug.Log("FOOD: Ordert: " + amount + " / Received: " + amount);
+            return amount;
+        }
+
+        if (amount > availableFood)
+        {
+            float tmp = availableFood;
+            availableFood = 0f;
+            ecoSystemManager.setAvailableFood(-tmp);
+            scaleFood();
+            //Debug.Log("FOOD: Ordert: " + amount + " / Received: " + tmp);
+            gameObject.layer = 2;
+            gameObject.SetActive(false);
+            return tmp;
+        }
+
+        gameObject.layer = 2;
+        gameObject.SetActive(false);
+        return 0f;
+    }
+
+    public float checkAmount()
+    {
+        return availableFood;
+    }
+
+
+
+}
---
+++ Assets/Scripts/EcoSystemManager/FoodManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class FoodManager : MonoBehaviour
+{
+    private Vector3 target;
+    public GameObject Food;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            dropFood();
+        }
+    }
+
+    public void dropFood()
+    {
+        target = new Vector3(Random.Range(-20, 20), Random.Range(3, 15), Random.Range(-20, 20));
+        Instantiate(Food, target, Quaternion.identity);
+    }
+}
