dcd3a713f25cad819a0459b4defdf292453bf2c8
Andreas Fuchs <info@anfuchs.de>
2019-12-22 18:20:29 +0100

    eco system manager: calculates, displays and controlls the balance

---
+++ Assets/EcoSystemManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EcoSystemManager : MonoBehaviour
+{
+    public int fishCount;
+    public float foodDemand;
+    public float availableFood;
+    public int diedFishes;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        availableFood = 0;
+        foodDemand = 0;
+        fishCount = 0;
+        diedFishes = 0;
+    }
+
+
+    public void setFoodDemand(float amount)
+    {
+        foodDemand = amount / fishCount;
+    }
+
+    public void setAvailableFood(float amount)
+    {
+        availableFood += amount;
+    }
+
+    public void setFishCount(int amount)
+    {
+        fishCount = amount;
+    }
+
+    public void addDiedFish()
+    {
+        diedFishes++;
+        fishCount--;
+    }
+}
