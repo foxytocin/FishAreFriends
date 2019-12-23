c4a11cbcbb1e503cf07abe4675673f078b548606
Florian Eder <florian.eder97@gmail.com>
2019-12-23 12:59:31 +0100

    Predator avoids wall and obstacle

---
+++ Assets/Scripts/Predator/Predator.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Predator : MonoBehaviour
+{
+
+
+    // State
+    [HideInInspector]
+    public Vector3 position;
+    [HideInInspector]
+    public Vector3 forward;
+    Vector3 velocity;
+
+    Transform cachedTransform;
+
+    float minSpeed = 1;
+    float maxSpeed = 3;
+    float maxSteerForce = 2;
+    float avoidCollisionWeight = 2;
+    float boundsRadius = 0.27f;
+    float collisionAvoidDst = 5;
+    LayerMask obstacleMask;
+
+    // Debug
+    bool showDebug = true;
+
+
+    void Awake()
+    {
+        cachedTransform = transform;
+        
+        obstacleMask = LayerMask.GetMask("Wall", "Obstacle");
+    }
+
+    public void Start()
+    {
+        position = cachedTransform.position;
+        forward = cachedTransform.forward;
+
+        float startSpeed = (minSpeed + maxSpeed) / 2;
+        velocity = transform.forward * startSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 acceleration = Vector3.zero;
+
+        if (IsHeadingForCollision())
+        {
+            Vector3 collisionAvoidDir = ObstacleRays();
+            Vector3 collisionAvoidForce = SteerTowards(collisionAvoidDir) * avoidCollisionWeight;
+            acceleration += collisionAvoidForce;
+        }
+
+        velocity += acceleration * Time.deltaTime;
+        float speed = velocity.magnitude;
+        Vector3 dir = velocity / speed;
+        speed = Mathf.Clamp(speed, minSpeed, maxSpeed);
+        velocity = dir * speed;
+
+        cachedTransform.position += velocity * Time.deltaTime;
+        cachedTransform.forward = dir;
+        position = cachedTransform.position;
+        forward = dir;
+
+    }
+
+
+    bool IsHeadingForCollision()
+    {
+        if (showDebug)
+        {
+            Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+            Debug.DrawRay(position, forward, Color.green);
+        }
+
+
+        RaycastHit hit;
+        if (Physics.SphereCast(position, boundsRadius, forward, out hit, collisionAvoidDst, obstacleMask))
+        {
+            return true;
+        }
+        else { }
+        return false;
+    }
+
+    Vector3 ObstacleRays()
+    {
+        Vector3[] rayDirections = BoidHelper.directions;
+
+
+        for (int i = 0; i < rayDirections.Length; i++)
+        {
+
+            Vector3 dir = cachedTransform.TransformDirection(rayDirections[i]);
+            Ray ray = new Ray(position, dir);
+
+            if (showDebug)
+            {
+                Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+                Debug.DrawRay(position, forward, Color.green);
+            }
+
+            if (!Physics.SphereCast(ray, boundsRadius, collisionAvoidDst, obstacleMask))
+            {
+                return dir;
+            }
+        }
+
+        return forward;
+    }
+
+    Vector3 SteerTowards(Vector3 vector)
+    {
+        Vector3 v = vector.normalized * maxSpeed - velocity;
+        return Vector3.ClampMagnitude(v, maxSteerForce);
+    }
+}
