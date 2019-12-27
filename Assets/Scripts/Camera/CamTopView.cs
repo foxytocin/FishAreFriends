fe2fde4b0130d263e0d8b571957b1452a8dc4d42
Andreas Fuchs <info@anfuchs.de>
2019-12-27 19:46:39 +0100

    first test of post-effect-controll from code

---
+++ Assets/Scripts/Camera/CamTopView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CamTopView : MonoBehaviour
+{
+
+    MapGenerator mapGenerator;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mapGenerator = FindObjectOfType<MapGenerator>();
+        centerOverTank();
+    }
+
+    private void centerOverTank()
+    {
+        Vector3 center = mapGenerator.mapSize / 2;
+        center += new Vector3(0, mapGenerator.mapSize.y / 2, 0);
+        transform.position = center;
+    }
+}
