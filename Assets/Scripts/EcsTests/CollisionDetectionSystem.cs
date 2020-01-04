c1adf1b37a736fb4ed5cb6828cc6e2de11cab5ff
Florian Eder <florian.eder97@gmail.com>
2020-01-04 10:51:46 +0100

    Added class and methode to check neighbourhood quadrant boxes. Later use this functionality to calculate boid stuff.

---
+++ Assets/Scripts/EcsTests/CollisionDetectionSystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class CollisionDetectionSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        
+         Entities.ForEach((Entity entity, ref Translation translation, ref QuadrantEntity quadrantEntity) => {
+
+             // get all keys, that should be considered
+             List<int> hashMapKeyList = QuadrantSystem.GetHashKeysForAroundData(translation.Value);
+             
+             foreach(int hashMapKey in hashMapKeyList)
+             {          
+                 QuadrantData quadrantData;
+                 NativeMultiHashMapIterator<int> nativeMultiHashMapIterator;
+                 if (QuadrantSystem.quadrantMultiHashMap.TryGetFirstValue(hashMapKey, out quadrantData, out nativeMultiHashMapIterator))
+                 {
+                     do
+                     {
+                         if (quadrantData.entity.Equals(entity))
+                             continue;
+
+                         // implement here boid stuff
+                         // TODO
+
+
+
+                     } while (QuadrantSystem.quadrantMultiHashMap.TryGetNextValue(out quadrantData, ref nativeMultiHashMapIterator));
+                 }
+             }
+
+             
+
+         });
+        
+
+
+
+    }
+}
