2ddc77d759e6ee4c01e0914dfc348943a913a4f2
Andreas Fuchs <info@anfuchs.de>
2020-01-04 02:01:36 +0100

    Rotation Test

---
+++ Assets/Scripts/EcsTests/RotationSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+
+public class RotationSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Rotate rotate, ref RotationEulerXYZ euler) =>
+        {
+            euler.Value.y += rotate.radiansPerSeconds * Time.DeltaTime;
+        });
+    }
+}
