a8541382dacc73904fb0dd6e6b52ba1af2b500d5
Florian Eder <florian.eder97@gmail.com>
2020-01-04 10:26:11 +0100

    Made roatation as parralel jobs.

--- Assets/Scripts/EcsTests/RotationSystem.cs
+++ Assets/Scripts/EcsTests/RotationSystem.cs
@@ -4,14 +4,41 @@ using UnityEngine;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Transforms;
+using Unity.Burst;
 
 public class RotationSystem : ComponentSystem
 {
     protected override void OnUpdate()
     {
+        /*
         Entities.ForEach((ref Rotate rotate, ref RotationEulerXYZ euler) =>
         {
             euler.Value.y += rotate.radiansPerSeconds * Time.DeltaTime;
         });
+        */
+
+
+        EntityQuery entityQuery = GetEntityQuery(typeof(Rotate), typeof(RotationEulerXYZ));
+
+        TranslateionJob translateionJob = new TranslateionJob
+        {
+            deltaTime = Time.DeltaTime,
+        };
+
+        JobHandle jobHandle = JobForEachExtensions.Schedule(translateionJob, entityQuery);
+        jobHandle.Complete();
+
+    }
+
+
+    [BurstCompile]
+    private struct TranslateionJob : IJobForEach<Rotate, RotationEulerXYZ>
+    {
+        public float deltaTime;
+
+        public void Execute(ref Rotate rotate, ref RotationEulerXYZ euler)
+        {
+            euler.Value.y += rotate.radiansPerSeconds * deltaTime;
+        }
     }
 }
