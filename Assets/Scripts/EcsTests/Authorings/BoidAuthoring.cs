47ecab188370b7a4249d5ecfbc514a61e2efff2e
Andreas Fuchs <info@anfuchs.de>
2020-01-06 03:32:56 +0100

    removesComponents: Translation and Rotation from Boid

--- Assets/Scripts/EcsTests/Authorings/BoidAuthoring.cs
+++ Assets/Scripts/EcsTests/Authorings/BoidAuthoring.cs
@@ -1,18 +1,16 @@
 ﻿using UnityEngine;
 using Unity.Entities;
+using Unity.Transforms;
 
 public class BoidAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 {
-    public float CellRadius = 8.0f;
-    public float SeparationWeight = 1.0f;
-    public float AlignmentWeight = 1.0f;
-    public float TargetWeight = 2.0f;
-    public float ObstacleAversionDistance = 30.0f;
-    public float MoveSpeed = 25.0f;
-
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
-        dstManager.AddComponentData(entity, new BoidComponent {});
+        dstManager.AddComponentData(entity, new BoidComponent { });
         dstManager.AddComponentData(entity, new QuadrantEntityComponent { typeOfObject = TypeOfObject.Boid });
+
+        // Remove default transform system components
+        dstManager.RemoveComponent<Translation>(entity);
+        dstManager.RemoveComponent<Rotation>(entity);
     }
 }
