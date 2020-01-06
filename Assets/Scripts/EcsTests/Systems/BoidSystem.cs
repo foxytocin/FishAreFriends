58a54d434ba6d4d7da1fd51b3586af00d737eba5
Andreas Fuchs <info@anfuchs.de>
2020-01-06 03:36:54 +0100

    LocalToWord: Alignment, Seperation etc. rule based movement (dummy data )

--- Assets/Scripts/EcsTests/Systems/BoidSystem.cs
+++ Assets/Scripts/EcsTests/Systems/BoidSystem.cs
@@ -8,88 +8,134 @@ using Unity.Mathematics;
 
 public class BoidSystem : ComponentSystem
 {
+    /*
+    public float CellRadius = 8.0f;
+    public float SeparationWeight = 1.0f;
+    public float AlignmentWeight = 1.0f;
+    public float TargetWeight = 2.0f;
+    public float ObstacleAversionDistance = 30.0f;
+    public float MoveSpeed = 25.0f;
+    */
+
+    float AlignmentWeight = 0.1f;
+    float SeparationWeight = 0.1f;
+    float TargetWeight = 0.1f;
+    float ObstacleAversionDistance = 0.1f;
+    float MoveSpeed = 2f;
+
+
     protected override void OnUpdate()
     {
-      
-         Entities.ForEach((Entity entity, ref Translation translation, ref QuadrantEntityComponent quadrantEntity, ref Rotation rotation, ref BoidComponent boidComponent) => {
 
-             float deltaTime = Time.DeltaTime;
+        Entities.ForEach((Entity entity, ref QuadrantEntityComponent quadrantEntity, ref LocalToWorld localToWorld, ref BoidComponent boidComponent) =>
+        {
 
-             // get all keys, that should be considered
-             List<int> hashMapKeyList = QuadrantSystem.GetHashKeysForAroundData(translation.Value);
+            float deltaTime = Time.DeltaTime;
 
-             QuadrantData nearestQuadrantData = default;
-             bool foundNearestQuadrantData = false;
-             
-             foreach(int hashMapKey in hashMapKeyList)
-             {          
-                 QuadrantData quadrantData;
-                 NativeMultiHashMapIterator<int> nativeMultiHashMapIterator;
-                 if (QuadrantSystem.quadrantMultiHashMap.TryGetFirstValue(hashMapKey, out quadrantData, out nativeMultiHashMapIterator))
-                 {
-                     do
-                     {
-                         if (quadrantData.entity.Equals(entity))
-                             continue;
+            // get all keys, that should be considered
+            List<int> hashMapKeyList = QuadrantSystem.GetHashKeysForAroundData(localToWorld.Position);
 
-                         if (!foundNearestQuadrantData)
-                         {
-                             nearestQuadrantData = quadrantData;
-                             foundNearestQuadrantData = true;
-                         }
+            QuadrantData nearestQuadrantData = default;
+            bool foundNearestQuadrantData = false;
 
-                             
+            int neighborCount = 0;
+            float3 alignment = new float3(0);
+            float3 separation = new float3(0);
+            float nearestObstacleDistance = 0;
 
-                         if(math.abs(math.distance(nearestQuadrantData.position, translation.Value)) > math.abs(math.distance(quadrantData.position, translation.Value)))
-                         {
-                             nearestQuadrantData = quadrantData;
-                         }
-                         
+            foreach (int hashMapKey in hashMapKeyList)
+            {
+                QuadrantData quadrantData;
+                neighborCount = 0;
 
-                         // implement here boid stuff
-                         // TODO
+                NativeMultiHashMapIterator<int> nativeMultiHashMapIterator;
+                if (QuadrantSystem.quadrantMultiHashMap.TryGetFirstValue(hashMapKey, out quadrantData, out nativeMultiHashMapIterator))
+                {
+                    do
+                    {
+                        if (quadrantData.entity.Equals(entity))
+                            continue;
 
+                        if (!foundNearestQuadrantData)
+                        {
+                            nearestQuadrantData = quadrantData;
+                            foundNearestQuadrantData = true;
+                        }
 
-
-                     } while (QuadrantSystem.quadrantMultiHashMap.TryGetNextValue(out quadrantData, ref nativeMultiHashMapIterator));
-                 }
-             }
-
-
-
-             // flipp direction
-             if (translation.Value.y > 100f || translation.Value.y < 0f)
-                 quadrantEntity.velocity.y *= -1;
-
-             if (translation.Value.z > 250f || translation.Value.z < -250f)
-                 quadrantEntity.velocity.z *= -1;
-
-             if (translation.Value.x > 250f || translation.Value.x < -250f)
-                 quadrantEntity.velocity.x *= -1;
+                        if (math.abs(math.distance(nearestQuadrantData.position, localToWorld.Position)) > math.abs(math.distance(quadrantData.position, localToWorld.Position)))
+                        {
+                            nearestObstacleDistance = math.distance(nearestQuadrantData.position, localToWorld.Position);
+                            nearestQuadrantData = quadrantData;
+                            neighborCount = 1;
+                            alignment = nearestQuadrantData.position;
+                            separation = nearestQuadrantData.position;
+                        }
 
 
-             // move
-             float3 currentPosition = translation.Value;
-             float3 targetPosition = currentPosition + quadrantEntity.velocity;
+                    } while (QuadrantSystem.quadrantMultiHashMap.TryGetNextValue(out quadrantData, ref nativeMultiHashMapIterator));
+                }
+            }
 
-             targetPosition = math.lerp(currentPosition, targetPosition, 0.5f * deltaTime);
-             translation.Value = targetPosition;
+            // todo dummy data
+            float3 forward = localToWorld.Forward;
+            float3 currentPosition = localToWorld.Position;
+            float3 nearestObstaclePosition = nearestQuadrantData.position;
+            float3 nearestTargetPosition = nearestQuadrantData.position;
+
+            // correct rules
+            float3 alignmentResult = AlignmentWeight * math.normalizesafe((alignment / neighborCount) - forward);
+            float3 separationResult = SeparationWeight * math.normalizesafe((currentPosition * neighborCount) - separation);
+            float3 targetHeading = TargetWeight * math.normalizesafe(nearestTargetPosition - currentPosition);
+
+            float3 obstacleSteering = currentPosition - nearestObstaclePosition;
+            float3 avoidObstacleHeading = (nearestObstaclePosition + math.normalizesafe(obstacleSteering) * ObstacleAversionDistance) - currentPosition;
+
+            float3 nearestObstacleDistanceFromRadius = nearestObstacleDistance - ObstacleAversionDistance;
+            float3 normalHeading = math.normalizesafe(alignmentResult + separationResult + targetHeading);
+            float3 targetForward = math.select(normalHeading, avoidObstacleHeading, nearestObstacleDistanceFromRadius < 0);
+
+            float3 nextHeading = math.normalizesafe(forward + deltaTime * (targetForward - forward));
+
+            // correct placement
+            localToWorld = new LocalToWorld
+            {
+                Value = float4x4.TRS(
+                    new float3(localToWorld.Position + (nextHeading * MoveSpeed * deltaTime)),
+                    quaternion.LookRotationSafe(nextHeading, math.up()),
+                    new float3(1.0f, 1.0f, 1.0f))
+            };
+
+            /*
+            // flipp direction
+            if (translation.Value.y > 100f || translation.Value.y < 0f)
+                quadrantEntity.velocity.y *= -1;
+
+            if (translation.Value.z > 250f || translation.Value.z < -250f)
+                quadrantEntity.velocity.z *= -1;
+
+            if (translation.Value.x > 250f || translation.Value.x < -250f)
+                quadrantEntity.velocity.x *= -1;
 
 
-             // rotate
-             float3 lookVector = targetPosition - currentPosition;
-             quaternion rotationValue = math.slerp(rotation.Value, quaternion.LookRotationSafe(lookVector, math.up()), 0.75f * deltaTime);
-             //quaternion.LookRotationSafe(lookVector, math.up()); //Quaternion.Lerp(Quaternion.LookRotation(lookVector), 0.5f * deltaTime * 3);
-             rotation.Value = rotationValue;
+            // move
+            float3 currentPosition = translation.Value;
+            float3 targetPosition = currentPosition + quadrantEntity.velocity;
+
+            targetPosition = math.lerp(currentPosition, targetPosition, 0.5f * deltaTime);
+            translation.Value = targetPosition;
+
+
+            // rotate
+            float3 lookVector = targetPosition - currentPosition;
+            quaternion rotationValue = math.slerp(rotation.Value, quaternion.LookRotationSafe(lookVector, math.up()), 0.75f * deltaTime);
+            //quaternion.LookRotationSafe(lookVector, math.up()); //Quaternion.Lerp(Quaternion.LookRotation(lookVector), 0.5f * deltaTime * 3);
+            rotation.Value = rotationValue;
+            */
+
+        });
 
 
 
-
-
-         });
-        
-
-    
 
     }
 }
--- Assets/Scripts/EcsTests/Systems/PlayerMovementSystem.cs
+++ Assets/Scripts/EcsTests/Systems/PlayerMovementSystem.cs
@@ -5,22 +5,26 @@ using Unity.Mathematics;
 
 public class PlayerMovementSystem : ComponentSystem
 {
-        // Key events
-        bool upKeyPressed = false;
-        bool downKeyPressed = false;
-        bool leftKeyPressed = false;
-        bool rightKeyPressed = false;
-        float3 forward;
-        float3 newTranslation;
+    // Key events
+    bool upKeyPressed = false;
+    bool downKeyPressed = false;
+    bool leftKeyPressed = false;
+    bool rightKeyPressed = false;
 
+    float AlignmentWeight = 1f;
+    float SeparationWeight = 1f;
+    float TargetWeight = 1f;
+    float MoveSpeed = 3f;
+    float ObstacleAversionDistance = 1;
 
-    float3 lookW = new float3(200, 40, 200);
 
 
     protected override void OnUpdate()
     {
+        float deltaTime = Time.DeltaTime;
+
         EntityQuery entityQuery = GetEntityQuery(typeof(InputComponent), typeof(PlayerComponent), typeof(LocalToWorld));
-        
+
         Entities.ForEach((Entity entity, ref InputComponent inputComponent, ref PlayerComponent playerComponent, ref LocalToWorld localToWorld) =>
         {
 
@@ -33,7 +37,7 @@ public class PlayerMovementSystem : ComponentSystem
                 upKeyPressed = true;
             if (Input.GetKeyDown(KeyCode.S))
                 downKeyPressed = true;
- 
+
             // key downs
             if (Input.GetKeyUp(KeyCode.A))
                 leftKeyPressed = false;
@@ -54,7 +58,54 @@ public class PlayerMovementSystem : ComponentSystem
             if (downKeyPressed)
                 playerComponent.velocity = new float3(0, 1, 0) * playerComponent.speed;
 
+            float3 forward = localToWorld.Forward;
+            float3 currentPosition = localToWorld.Position;
 
+            /*
+            var cellIndex = cellIndices[entityInQueryIndex];
+            var neighborCount = cellCount[cellIndex];
+            var alignment = cellAlignment[cellIndex];
+            var separation = cellSeparation[cellIndex];
+            var nearestObstacleDistance = cellObstacleDistance[cellIndex];
+            var nearestObstaclePositionIndex = cellObstaclePositionIndex[cellIndex];
+            var nearestTargetPositionIndex = cellTargetPositionIndex[cellIndex];
+            var nearestObstaclePosition = copyObstaclePositions[nearestObstaclePositionIndex];
+            var nearestTargetPosition = copyTargetPositions[nearestTargetPositionIndex];
+            */
+
+            var neighborCount = 1;
+            var alignment = new float3(1, 1, 1);
+            var separation = new float3(0, 0, 0);
+            var nearestObstacleDistance = new float3(1, 1, 1);
+            var nearestTargetPositionIndex = new float3(1, 1, 1);
+            var nearestObstaclePosition = currentPosition + new float3(-1, -1, -1);
+            var nearestTargetPosition = currentPosition + new float3(1, 1, 1);
+
+
+            var alignmentResult = AlignmentWeight * math.normalizesafe((alignment / neighborCount) - forward);
+            var separationResult = SeparationWeight * math.normalizesafe((currentPosition * neighborCount) - separation);
+            var targetHeading = TargetWeight * math.normalizesafe(nearestTargetPosition - currentPosition);
+
+            var obstacleSteering = currentPosition - nearestObstaclePosition;
+            var avoidObstacleHeading = (nearestObstaclePosition + math.normalizesafe(obstacleSteering) * ObstacleAversionDistance) - currentPosition;
+
+            var nearestObstacleDistanceFromRadius = nearestObstacleDistance - ObstacleAversionDistance;
+            var normalHeading = math.normalizesafe(alignmentResult + separationResult + targetHeading);
+            var targetForward = math.select(normalHeading, avoidObstacleHeading, nearestObstacleDistanceFromRadius < 0);
+
+            var nextHeading = math.normalizesafe(forward + deltaTime * (targetForward - forward));
+
+            nextHeading += new float3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+            localToWorld = new LocalToWorld
+            {
+                Value = float4x4.TRS(
+                    new float3(localToWorld.Position + (nextHeading * MoveSpeed * deltaTime)),
+                    quaternion.LookRotationSafe(nextHeading, math.up()),
+                    new float3(1.0f, 1.0f, 1.0f))
+            };
+
+            /*
             if (Input.GetKeyDown(KeyCode.Q))
             {
                 playerComponent.speed -= 0.5f;
@@ -71,7 +122,7 @@ public class PlayerMovementSystem : ComponentSystem
             float3 nextHeading = currentPosition + playerComponent.velocity;
 
             nextHeading = math.lerp(currentPosition, nextHeading, 0.5f * Time.DeltaTime * playerComponent.speed);
-          
+
 
             localToWorld = new LocalToWorld
             {
@@ -80,7 +131,7 @@ public class PlayerMovementSystem : ComponentSystem
                             quaternion.LookRotationSafe(nextHeading, math.up()),
                             new float3(20.0f, 50.0f, 20.0f))
             };
-
+*/
 
 
         });
