64ae7c74396629709d085e4dd7611f111b0ec42e
Andreas Fuchs <info@anfuchs.de>
2020-01-08 04:14:49 +0100

    highly optimized multithreading

--- Assets/Scripts/Boids/Boid.cs
+++ Assets/Scripts/Boids/Boid.cs
@@ -142,33 +142,9 @@ public class Boid : MonoBehaviour
     }
 
 
-    public void UpdateBoid()
+    public void UpdateBoid(Vector3 acceleration_)
     {
-
-        // movement
-        Vector3 acceleration = Vector3.zero;
-        if (target != null)
-        {
-            Vector3 offsetToTarget = (target.position - position);
-            acceleration = SteerTowards(offsetToTarget) * settings.targetWeight;
-        }
-
-        if (numPerceivedFlockmates != 0)
-        {
-            centreOfFlockmates /= numPerceivedFlockmates;
-
-            Vector3 offsetToFlockmatesCentre = (centreOfFlockmates - position);
-
-            var alignmentForce = SteerTowards(avgFlockHeading) * settings.alignWeight;
-            var cohesionForce = SteerTowards(offsetToFlockmatesCentre) * settings.cohesionWeight;
-            var seperationForce = SteerTowards(avgAvoidanceHeading) * settings.seperateWeight;
-
-            acceleration += alignmentForce;
-            acceleration += cohesionForce;
-            acceleration += seperationForce;
-        }
-
-
+        Vector3 acceleration = acceleration_;
 
         if (IsHeadingForCollision())
         {
