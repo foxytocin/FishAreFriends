5fce1bdc43059bf316f7009f30d9febb312f291a
Andreas Fuchs <info@anfuchs.de>
2020-01-13 04:32:12 +0100

    basic tutorial-system

--- Assets/Tutorial.cs
+++ Assets/Tutorial.cs
@@ -21,17 +21,18 @@ public class Tutorial : MonoBehaviour
 
     private IEnumerator BasicTutorial() {
 
+        // splash
         guiOverlay.DisplayMainMessage("FISH ARE FRIENDS", 2);
 
-
+        // start
         yield return new WaitForSeconds(4);
         guiOverlay.DisplayMainMessage("Los geht's", 2);
 
-
+        // find friends
         yield return new WaitForSeconds(8);
         guiOverlay.DisplayMainMessage("Du bist ganz allein. Finde neue Freunde, indem Du in ihre Naehe schwimmst", 8);
 
-
+        // wait until 10 friend are found
         int count = 0;
         while(count < 10) {
             count = leaderPlayer.GetSwarmSize();
@@ -39,7 +40,7 @@ public class Tutorial : MonoBehaviour
         }
         guiOverlay.DisplayMainMessage("Sehr gut! Du hast " +count+ " neue Freunde eingesammelt", 6);
 
-
+        // beware the shark
         yield return new WaitForSeconds(80);
         guiOverlay.DisplayMainMessage("Sei vorsichtig! Hier soll irgendwo ein Hai sein", 8);
 
