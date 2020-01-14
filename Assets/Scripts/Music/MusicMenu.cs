e1bca78ba2b214d3dac6aec160303bf56c81da0e
Andreas Fuchs <info@anfuchs.de>
2020-01-14 23:48:58 +0100

    play, pause and fade-effects for menu-music

---
+++ Assets/Scripts/Music/MusicMenu.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicMenu : MonoBehaviour
+{
+    private AudioSource audioMusic;
+    private AudioEchoFilter audioEcho;
+    private float volume = 0f;
+    public float speed = 0.8f;
+    public float minVolume = 0.2f;
+
+    Coroutine playAndFade = null;
+    Coroutine fadeAndPause = null;
+
+    void Awake()
+    {
+        audioMusic = GetComponent<AudioSource>();
+        audioEcho = GetComponent<AudioEchoFilter>();
+        audioMusic.volume = volume;
+    }
+
+
+    public void StartMenuMusic()
+    {
+        playAndFade = StartCoroutine(PlayAndFade());
+    }
+
+    public void StopMenuMusic()
+    {
+        fadeAndPause = StartCoroutine(FadeAndPause());
+    }
+
+
+    private IEnumerator PlayAndFade()
+    {
+        if (fadeAndPause != null)
+            StopCoroutine(fadeAndPause);
+
+        audioEcho.enabled = false;
+        audioMusic.Play();
+
+        while (volume < 1f)
+        {
+            volume += Time.deltaTime * speed;
+            audioMusic.volume = volume;
+            yield return new WaitForEndOfFrame();
+        }
+
+        volume = 1f;
+        audioMusic.volume = volume;
+    }
+
+
+    private IEnumerator FadeAndPause()
+    {
+        if (playAndFade != null)
+            StopCoroutine(playAndFade);
+
+
+        while (volume > minVolume)
+        {
+            if (volume < ((1 + minVolume) / 2f))
+                audioEcho.enabled = true;
+
+            volume -= Time.deltaTime * (speed / 3f);
+            audioMusic.volume = volume;
+            yield return new WaitForEndOfFrame();
+        }
+
+        audioMusic.Pause();
+        volume = minVolume;
+        audioMusic.volume = volume;
+    }
+}
