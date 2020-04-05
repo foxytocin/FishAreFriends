76456b853ee6bad9ac0d0273fd98ad5ce756c907
Andreas Fuchs <info@anfuchs.de>
2020-04-05 18:13:09 +0200

    new settings-menu for resolustion, quality and inverted controlls

---
+++ Assets/Scripts/GUI/SettingQuality.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingQuality : MonoBehaviour
+{
+
+    public Dropdown qualityDropdown;
+
+
+    void Start()
+    {
+
+        qualityDropdown.value = QualitySettings.GetQualityLevel();
+        qualityDropdown.RefreshShownValue();
+    }
+
+    public void setQuality()
+    {
+        QualitySettings.SetQualityLevel(qualityDropdown.value);
+    }
+}
