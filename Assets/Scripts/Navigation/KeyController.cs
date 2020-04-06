f7daf937ce10701d9f7554fabdf31f870d725d48
Andreas Fuchs <info@anfuchs.de>
2020-04-06 04:28:17 +0200

    massive improved keyController removes 14 if-cases

--- Assets/Scripts/Navigation/KeyController.cs
+++ Assets/Scripts/Navigation/KeyController.cs
@@ -4,7 +4,6 @@ using System.Collections;
 
 public class KeyController : MonoBehaviour
 {
-
     FlowStream flowStream;
     CellGroups cellGroups;
     GuiOverlay guiOverlay;
@@ -26,11 +25,7 @@ public class KeyController : MonoBehaviour
     public float collisionAvoidDst = 5f;
     public LayerMask obstacleMask;
 
-    // Key events
-    private bool upKeyPressed = false;
-    private bool downKeyPressed = false;
-    private bool leftKeyPressed = false;
-    private bool rightKeyPressed = false;
+
     private bool invertedControlls = false;
     private bool invertingSemaphor = false;
     private SettingInvertControlls settingInvertControlls;
@@ -89,8 +84,6 @@ public class KeyController : MonoBehaviour
             guiOverlay.DisplayMainMessage("Invertierte Steuerung aktiviert", 1, GuiOverlay.MessageType.info);
         }
 
-        upKeyPressed = false;
-        downKeyPressed = false;
         yield return new WaitForSeconds(0.5f);
         invertingSemaphor = false;
     }
@@ -100,8 +93,7 @@ public class KeyController : MonoBehaviour
     {
         if (guiOverlay.gameStatus == GuiOverlay.GameStatus.inGame)
         {
-
-            cellGroups.SetPlayerCell(transform.position);
+            //cellGroups.SetPlayerCell(transform.position);
 
             if (!invertingSemaphor && Input.GetKeyDown(KeyCode.I))
                 StartCoroutine(InvertControll());
@@ -128,77 +120,24 @@ public class KeyController : MonoBehaviour
                 if (Input.GetKeyDown(KeyCode.Q))
                 {
                     speed -= 0.5f;
-                    //Debug.Log("KeyDown Q: " + speed);
                 }
-                if (Input.GetKeyDown(KeyCode.E))
+                else if (Input.GetKeyDown(KeyCode.E))
                 {
                     speed += 0.5f;
-                    //Debug.Log("KeyDown E: " + speed);
                 }
             }
 
             speed = Mathf.Clamp(speed, 0.00001f, maxSpeed);
             velocity = dir * speed;
 
-            // handle key events
             if (!isHeadingForCollision)
             {
-                // key ups
-                if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-                    leftKeyPressed = true;
-                if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-                    rightKeyPressed = true;
+                Vector3 right = transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+                Vector3 up = transform.up * Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
-                if (!invertedControlls)
-                {
-                    if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-                        upKeyPressed = true;
-                    if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-                        downKeyPressed = true;
-                }
-                else
-                {
-                    if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-                        upKeyPressed = true;
-                    if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-                        downKeyPressed = true;
-                }
+                velocity += right;
+                velocity += (!invertedControlls) ? up : -up;
             }
-
-            // key downs
-            if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
-                leftKeyPressed = false;
-            if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
-                rightKeyPressed = false;
-
-            if (!invertedControlls)
-            {
-                if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow))
-                    upKeyPressed = false;
-                if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
-                    downKeyPressed = false;
-            }
-            else
-            {
-                if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
-                    upKeyPressed = false;
-                if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow))
-                    downKeyPressed = false;
-            }
-
-            if (!isHeadingForCollision)
-            {
-
-                if (leftKeyPressed)
-                    velocity -= transform.right * Time.deltaTime * speed;
-                if (rightKeyPressed)
-                    velocity += transform.right * Time.deltaTime * speed;
-                if (upKeyPressed)
-                    velocity += transform.up * Time.deltaTime * speed;
-                if (downKeyPressed)
-                    velocity -= transform.up * Time.deltaTime * speed;
-            }
-
 
             cachedTransform.position += velocity * Time.deltaTime;
             cachedTransform.forward = dir;
